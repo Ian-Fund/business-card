0e1cdc8855b3a5eeb1e6b47daf8e9941756d3a38
Ian Fund <ianfund117@gmail.com>
2022-06-10 17:35:46 -0500

    Left button copies email. No working popup. Right button opens new tab to linkedin page

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -4,11 +4,15 @@ import "../CSS/Info.css";
 import headshot from "../Images/Headshot.jpg";
 import Popup from "reactjs-popup";
 
-const Modal = () => (
-  <Popup trigger={<button className="button"> Open Modal </button>} modal>
-    <span> Modal content </span>
-  </Popup>
-);
+this.state = {
+  message: "",
+};
+
+const handleEvent = (event) => {
+  if (event.type === "mousedown") {
+    this.setState({ message: "Copied to clipboard!" });
+  }
+};
 export default function Info() {
   return (
     <div className="infoSection">
@@ -18,12 +22,22 @@ export default function Info() {
         <h2>Software Developer</h2>
       </div>
       <div id="button-section">
-        <button className="button">Left</button>
-        <button className="button">Right</button>
-        <Popup trigger={<button className="button"> Open Modal </button>} modal>
-          {" "}
-          <span> Modal content </span>{" "}
-        </Popup>
+        <button
+          className="button"
+          onClick={() => {
+            navigator.clipboard.writeText("ianfund117@gmail.com");
+          }}
+          onMouseDown={this.handleEvent}
+        >
+          Left
+        </button>
+        <a
+          className="button"
+          href="https://www.linkedin.com/in/ian-fund/"
+          target="_blank"
+        >
+          <button>Right</button>
+        </a>
       </div>
     </div>
   );
