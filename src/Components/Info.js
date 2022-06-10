86af745465f93e2b462dc809cf4c88775256289c
Ian Fund <ianfund117@gmail.com>
2022-06-09 21:31:05 -0500

    Addded buttons that don't work. Centered some content

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,5 +1,30 @@
 import React from "react";
 import "../CSS/Card.css";
+import "../CSS/Info.css";
+import headshot from "../Images/Headshot.jpg";
+import Popup from "reactjs-popup";
+
+const Modal = () => (
+  <Popup trigger={<button className="button"> Open Modal </button>} modal>
+    <span> Modal content </span>
+  </Popup>
+);
 export default function Info() {
-  return <div className="infoSection">Top</div>;
+  return (
+    <div className="infoSection">
+      <img id="headshot" src={headshot} alt="headshmmot" />
+      <div id="name-job-section">
+        <h1>Ian Fund</h1>
+        <h2>Software Developer</h2>
+      </div>
+      <div id="button-section">
+        <button className="button">Left</button>
+        <button className="button">Right</button>
+        <Popup trigger={<button className="button"> Open Modal </button>} modal>
+          {" "}
+          <span> Modal content </span>{" "}
+        </Popup>
+      </div>
+    </div>
+  );
 }
